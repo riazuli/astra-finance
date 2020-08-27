0a96d306692efb0df1672456dc893392160c2424
Riazul Islam <ri64627@gmail.com>
2020-08-27 15:01:12 +0600

    added more changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
-(function($) {
+(function ($) {
   "use strict";
 
-  jQuery(document).ready(function($) {
-    
+  jQuery(document).ready(function ($) {
+    $(window).scroll(function () {
+      if ($(window).scrollTop() >= 300) {
+        $(".header-area").addClass("sticky");
+      } else {
+        $(".header-area").removeClass("sticky");
+      }
+    });
   });
 })(jQuery);
